1faf4687feb7683fab94f0fc67f2f580c374dce8
kiasNostieDev <akinthiyasrinath@gmail.com>
2021-01-17 14:11:14 +0530

    Still @pshot

--- shootout.cpp
+++ shootout.cpp
@@ -7,11 +7,12 @@
 using namespace std;
 
 int main(){
-    int T,N;
-    scanf("%d",&T);
+    int T=0,N=0;
+    cin>>T;
+    cout<<T;
     while(T--){
-        queue<pair<int,int>> penalty;
-        int result, a=0, b=0, count=0;
+        queue<int> penalty;
+        int result, a=0, b=0;
         scanf("%d",&N);
         for(int i=0;i<2*N;i++){
             int c;
@@ -20,23 +21,39 @@ int main(){
             b = i%2==0 ? b+c : b;
             result = a>b ? 0 : 1;
             result = a==b ? -1 : result;
-            penalty.push(make_pair(i%2!=0 ? 1 : 0,c));
+            penalty.push(c);
         }
-        int ca=0,cb=0,ra=N-1,rb=N-1,r;
-        while(true){
-            if(penalty.empty())break;
-            pair<int,int> poppedVal = penalty.front();
-            penalty.pop();
-            ca = poppedVal.first == 0 ? ca+poppedVal.second : ca;
-            cb = poppedVal.first == 1 ? cb+poppedVal.second : cb;
-            ra = poppedVal.first == 0 ? ra-count+ca : ra;
-            rb = poppedVal.first == 1 ? rb-count+cb : rb;
-            if(ca>rb)r=0;
-            else if(cb>ra)r=1;
-            else r=-1;
-            if(r==result)break;
-            count++;
+
+        int A=0,B=0;
+        for(int i=1;i<=2*N;i++){
+            if(i%2!=0){
+                int front = penalty.front();
+                penalty.pop();
+                A+=front;
+                int letsSayB = (N-(i/2)-1);
+                cout<<i<<"i ";
+                cout<<front<<"Front "<<A<<"A ";
+                cout<<letsSayB+B<<"LSSB ";
+                cout<<endl;
+                if(A > letsSayB+B && result == 0){
+                    cout<<i+1<<"Jadhav"<<endl;
+                    break;
+                }
+            }else{
+                int front = penalty.front();
+                penalty.pop();
+                B+=front;
+                int letsSayA = (N-(i/2)-1);
+                cout<<i<<"i ";
+                cout<<front<<"Front "<<B<<"B ";
+                cout<<letsSayA+A<<"LSSA ";
+                cout<<endl;
+                if(B > letsSayA+A && result == 1){
+                    cout<<i+1<<"Jadhav"<<endl;
+                    break;
+                }
+            }
         }
-        cout<<count<<endl;
+        cout<<N<<endl;
     }
 }
