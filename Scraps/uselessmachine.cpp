1ea32ac68fb506f50a442d55bc64ed630e54d6b7
kiasNostieDev <akinthiyasrinath@gmail.com>
2021-01-18 16:14:45 +0530

    @STUPMACH

--- Scraps/uselessmachine.cpp
+++ Scraps/uselessmachine.cpp
@@ -5,15 +5,24 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+inline int fastIO(){
+    char c = getchar_unlocked();
+    int x=0;
+    for(;c<'0'||c>'9';c=getchar_unlocked());
+    for(;c>='0' && c<='9';c=getchar_unlocked()){
+        x = (x<<1)+(x<<3)+c-'0';
+    }
+    return x;
+}
 
 int main(){
     int T,N;
-    scanf("%d",&T);
+    T=fastIO();
     while(T--){
-        scanf("%d",&N);
+        N=fastIO();
         int capacity[N], checkpoint=N,sum=0,c=N,min=1001;
         for(int i=0;i<N;i++){
-            scanf("%d",&capacity[i]);
+            capacity[i]=fastIO();
             if(min>capacity[i])min=capacity[i];
             sum+=min;
 //            if(capacity[i]==0)checkpoint=i;
