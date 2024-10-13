7d79f4de6f2deb53c61376d0464c3375e638cbc0
kiasNostieDev <akinthiyasrinath@gmail.com>
2024-10-13 11:47:17 +0530

    pronunciation code accepted answer update

--- Scraps/pronunciation.cpp
+++ Scraps/pronunciation.cpp
@@ -1,31 +1,38 @@
 // https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/EZSPEAK
 
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-	// your code goes here
-	unsigned int totalCases;
-	cin>>totalCases;
-	while(totalCases--){
-	    unsigned int caseLength, consonantCount = 0;
-	    cin>>caseLength;
-	    while(caseLength--){
-	        if(consonantCount >= 4){
-	            printf("NO\n");
-	            break;
-	        }
-	        unsigned int testCase;
-	        scanf("%c", &testCase);
-	       // 97, 101, 105, 111, and 11
-	        if((testCase == 105 || testCase == 97 || testCase == 111 || testCase == 101 || testCase == 117)){
-	            consonantCount = 0;
-	        } else {
-	            consonantCount += 1;
-	        }
-	    }
-	    if(consonantCount < 4){
-	        printf("YES\n");
-	    }
-	}
+  // your code goes here
+  unsigned int totalCases;
+  cin >> totalCases;
+  while (totalCases--) {
+    unsigned int caseLength, consonantCount = 0;
+    cin >> caseLength;
+    while (caseLength--) {
+      char testCase;
+      if (consonantCount >= 4) {
+        caseLength+=1;
+        while(caseLength--){
+            scanf(" %c", &testCase);
+        }
+        break;
+      }
+      scanf(" %c", &testCase);
+      // 97, 101, 105, 111, and 11
+      if (((int)testCase == 105 || (int)testCase == 97 ||
+           (int)testCase == 111 || (int)testCase == 101 ||
+           (int)testCase == 117)) {
+        consonantCount = 0;
+      } else {
+        consonantCount += 1;
+      }
+    }
+    if (consonantCount < 4) {
+      printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+  }
 }
